7268b1ed520d7474750c68b3db32402c88d75409
Чижов Виктор Сергеевич <vschizhov@msk.rn.ru>
2019-09-19 13:04:25 +0300

    project

---
+++ Consolidation.vue
@@ -0,0 +1,178 @@
+<template>
+	<div class="module-container">
+		<div class="module">
+			<div class="module-header">
+				<label>
+					<input type="checkbox" title="Выбрать все консолидации верхнего уровня">
+				</label>
+				<div class="display-consolidation">
+					<i class="fas fa-th-list"></i>
+					<i class="fas fa-table"></i>
+				</div>
+				<span title="Версия отчетности">Цель ВО:</span>
+				<select style="width: 12%; margin: 0 10px">
+					<option>test</option>
+					<option>test 2</option>
+					<option>test 3</option>
+				</select>
+				<span title="Версия загрузки или расчета">ВЗ/Р:</span>
+				<select style="width: 12%; margin: 0 10px">
+					<option>test</option>
+					<option>test 2</option>
+					<option>test 3</option>
+				</select>
+				<span title="Ретроспективная версия отчетности">РВО:</span>
+				<select style="width: 12%; margin: 0 10px">
+					<option>test</option>
+					<option>test 2</option>
+					<option>test 3</option>
+				</select>
+				<div class="settings">
+					<div class="buttons">
+						<i class="fas fa-download" title="Загрузить консолидацию из файла/ЕИСИПИМ"></i>
+						<i class="fas fa-check-double" title="Включить в существующую/новую консолидацию"></i>
+						<i class="fas fa-plus-circle" title="Создать новую консолидацию/консолидацию-папку"></i>
+						<i class="fas fa-edit" title="Редактировать название консолидации и флаг включения"></i>
+						<i class="fas fa-minus-circle" title="Удалить консолидацию без ее наполнения"></i>
+						<i class="fas fa-trash" title="Удалить консолидацию полностью"></i>
+						<i class="far fa-list-alt" title="Отобразить окно списка версий загрузки или расчета"></i>
+						<i class="fas fa-cog" title="Настроить состав реквизитов"></i>
+					</div>
+					<div class="indicator">
+						<i class="fas fa-circle" title="В работе" style="color: #EF1B1B"></i>
+						<i class="fas fa-circle" title="Внимание" style="color: #EED226"></i>
+						<i class="fas fa-circle" title="Ошибки ЭЭ" style="color: #F66AB4"></i>
+						<i class="fas fa-circle" title="Готов к описанию НО" style="color: #644F00"></i>
+						<i class="fas fa-circle" title="Готов" style="color: #1F5C03"></i>
+					</div>
+				</div>
+			</div>
+			<div class="module-content"></div>
+		</div>
+		<div class="module-info-panel"></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data: function() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	@import '../styles.less';
+	
+	.module-container {
+		display: grid;
+        grid-template-columns: 3fr 1fr;
+        height: @module-container-height;
+
+        .module {
+        	.module-header {
+        		display: flex;
+                flex-flow: row nowrap;
+                align-items: center;
+                height: @header-height;
+                background-color: @color-grey-6;
+                border-bottom: 1px solid @color-grey-4;
+                user-select: none;
+
+                label {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					height: @header-height;
+					margin: 0;
+					padding-left: 10px;
+
+					input {
+						position: relative;
+						width: 20px;
+						height: 20px;
+						cursor: pointer;
+					}
+				}
+                
+                .display-consolidation {
+                	display: flex;
+					align-items: center;
+					padding: 0 10px;
+					color: @color-grey-4;
+					font-size: 20px;
+
+					i {
+						margin-right: 5px;
+
+						&:hover {
+							color: @color-grey-1;
+							cursor: pointer;
+						}
+					}
+				}
+
+				span {
+					font-weight: 600;
+					white-space: nowrap;
+					overflow: hidden;
+					text-overflow: ellipsis;
+				}
+
+				.settings {
+					display: flex;
+					flex-flow: row nowrap;
+					align-items: center;
+					height: @header-height;
+					margin-left: auto;
+					padding: 0 10px;
+
+					.buttons {
+						display: flex;
+						flex-flow: row nowrap;
+						align-items: center;
+						height: @header-height;
+						padding: 0 10px;
+
+						i {
+							margin-left: 10px;
+							font-size: 18px;
+							color: @color-grey-4;
+
+							&:hover {
+								color: @color-grey-1;
+								cursor: pointer;
+							}
+						}
+					}
+
+					.indicator {
+						display: flex;
+						flex-flow: row nowrap;
+						align-items: center;
+						padding: 3px 5px;
+						border: 1px solid @color-grey-4;
+						border-radius: 20px;
+
+						i {
+							color: @color-grey-5;
+							font-size: 14px;
+							margin: 0 2px;
+						}
+					}
+				}
+			}
+
+			.module-content {
+				
+			}
+        }
+
+        .module-info-panel {
+            border-top: 1px solid @color-grey-4;
+            border-left: 1px solid @color-grey-4;
+        }
+	}
+</style>
---
+++ modalDownloadCalculationVersions.vue
@@ -0,0 +1,192 @@
+<template>
+	<div class="modal fade" id="modalDownloadCalculationVersions" role="dialog">
+	    <div class="modal-dialog" role="document">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	            	<h1>Версии загрузки или расчета</h1>
+	            	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	            </div>
+				<div class="modal-body">
+					<div class="add-new-version-wrapper">
+						<div class="title">
+							<h3>Новая версия загрузки или отчета</h3>
+						</div>
+						<div class="form-group">
+							<label>Наименование</label>
+							<input type="text" class="form-control" placeholder="Введите наименование...">
+						</div>
+						<div class="form-group">
+							<label>Организационная единица</label>
+							<div class="input-group">
+								<select class="form-control">
+									<option>test</option>
+									<option>test 2</option>
+									<option>test 3</option>
+								</select>
+								<span>
+									<i class="fas fa-question"></i>
+								</span>
+							</div>
+						</div>
+						<button class="btn-add">Создать</button>
+					</div>
+				</div>
+	        </div>
+	    </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data: function() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	@import '../styles.less';
+
+	.modal-content {
+		width: 150%;
+		right: 25%;
+
+		.modal-header {
+			display: flex;
+			align-items: center;
+
+			h1 {
+				margin: 0;
+				font-size: 18px;
+			}
+		}
+
+		.modal-body {
+			padding-top: 5px;
+
+			.table-wrapper {
+				display: grid;
+				grid-template-columns: 1fr 1fr;
+				grid-column-gap: 5px;
+				margin-bottom: 20px;
+
+				.table-content {
+					.title {
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						padding: 5px 0;
+						font-weight: 600;
+					}
+
+					.search {
+						form {
+							position: relative;
+							width: 100%;
+
+							input {
+								width: 100%;
+								height: 30px;
+								background-color: @color-grey-6;
+								border: 1px solid @color-border-default;
+								border-bottom: none;
+								border-radius: 5px 5px 0 0;
+								padding-left: 10px;
+								color: @color-grey-3;
+								outline: none;
+							}
+
+							span {
+								display: flex;
+								justify-content: center;
+								align-items: center;
+								position: absolute; 
+								top: 0;
+								right: 0px;
+								width: 30px;
+								height: 30px;
+								border-radius: 0 5px 0 0;
+								background-color: @color-grey-2;
+								color: @color-grey-6;
+								cursor: pointer;
+
+								&:active {
+									background-color: @color-grey-3;
+								}
+							}
+						}
+					}
+
+					.table-container {
+						height: 50vh;
+						overflow-y: scroll;
+
+						table {
+							margin: 0;
+
+							td {
+								padding: 2px 10px;
+							}
+						}
+					}
+				}
+			}
+
+			.add-new-version-wrapper {
+				.title {
+					padding-bottom: 10px;
+					border-bottom: 1px dashed @color-border-default;
+
+					h3 {
+						margin: 0;
+						font-size: 18px;
+					}
+				}
+
+				.form-group {
+					margin: 10px 0;
+
+					.input-group {
+						span {
+							display: flex;
+							justify-content: center;
+							align-items: center;
+							padding: 0 12px;
+							border: 1px solid @color-border-default;
+							border-left: none;
+							border-radius: 0 5px 0 0;
+							background-color: @color-grey-2;
+							color: @color-grey-6;
+							cursor: pointer;
+
+							&:active {
+								background-color: @color-grey-3;
+							}
+						}
+					}
+				}
+
+				.btn-add {
+					padding: 5px 5em;
+					background-color: @color-grey-2;
+					border: 1px solid @color-grey-2;
+					border-radius: 10px;
+					cursor: pointer;
+					font-weight: 600;
+					color: @color-grey-6;
+					outline: none;
+
+					&:hover {
+						background-color: @color-grey-1;
+					}
+
+					&:active {
+						background-color: @color-grey-3;
+					}
+				}
+			}
+		}
+	}
+</style>
